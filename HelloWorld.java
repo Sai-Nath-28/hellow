2f9a2bb5eee4bc788f3472bf088a4ef8eca7e0f8
sainath <sainath21997@gmail.com>
2021-12-23 12:01:38 +0000

    message

--- HelloWorld.java
+++ HelloWorld.java
@@ -14,11 +14,9 @@
 
 public class HelloWorld {
   public static void main(String[] args) {
+    System.out.println("Hello, sainath");
     System.out.println("Hello, World");
     System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, sainath);
 
   }
 }
